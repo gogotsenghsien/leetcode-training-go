397694ba4d564e6f79a0d1283dcd0c4efc060975
Chris.T <gogotsenghsien@gmail.com>
2021-02-22 17:08:01 +0800

    Accomplish two-sum problem.

---
+++ two-sum/main.go
@@ -0,0 +1,13 @@
+package main
+
+// Problem URI: https://leetcode.com/problems/two-sum/
+func twoSum(nums []int, target int) []int {
+	records := map[int]int{}
+	for idx, val := range nums {
+		if v, ok := records[target-val]; ok {
+			return []int{v, idx}
+		}
+		records[val] = idx
+	}
+	return []int{-1, -1}
+}
