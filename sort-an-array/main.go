47e69f4c094526c093f090209ec0c5762dd2f164
Chris <gogotsenghsien@gmail.com>
2021-02-26 04:05:55 +0800

    Add merge sort solution in sort-an-array

--- sort-an-array/main.go
+++ sort-an-array/main.go
@@ -1,11 +1,31 @@
 package main
 
-func sortArray(nums []int) []int {
-	// 1. built-in sort method
-	//sort.Ints(nums)
-	//return nums
+import "sort"
 
-	// 2. bubble sort
+func sortArray(nums []int) []int {
+	n := 1 // choose the case you want
+
+	switch n {
+	case 1:
+		// 1. built-in sort method
+		return builtInSort(nums)
+	case 2:
+		// 2. bubble sort
+		return bubbleSort(nums)
+	case 4:
+		// 4. merge sort
+		return MergeSort(nums)
+	}
+
+	return []int{}
+}
+
+func builtInSort(nums []int) []int {
+	sort.Ints(nums)
+	return nums
+}
+
+func bubbleSort(nums []int) []int {
 	l := len(nums)
 	for i := 0; i < l; i++ {
 		for j := 0; j < l-1-i; j++ {
@@ -13,6 +33,40 @@ func sortArray(nums []int) []int {
 				nums[j], nums[j+1] = nums[j+1], nums[j]
 			}
 		}
+	}
+	return nums
+}
+
+func MergeSort(nums []int) []int {
+	if len(nums) > 1 {
+		mid := len(nums) / 2
+		l := nums[:mid]
+		r := nums[mid:]
+		l = sortArray(l)
+		r = sortArray(r)
+		i, j := 0, 0
+		res := make([]int, 0)
+
+		for i < len(l) && j < len(r) {
+			if l[i] < r[j] {
+				res = append(res, l[i])
+				i++
+			} else {
+				res = append(res, r[j])
+				j++
+			}
+		}
+
+		for i < len(l) {
+			res = append(res, l[i])
+			i++
+		}
+
+		for j < len(r) {
+			res = append(res, r[j])
+			j++
+		}
+		return res
 	}
 	return nums
 }
