2a3d3db9d90f377ddaaae1479e0e5bcf3668a2fc
Chris <gogotsenghsien@gmail.com>
2021-02-26 02:42:48 +0800

    Add bubble sort solution

--- sort-an-array/main.go
+++ sort-an-array/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "sort"
-
 func sortArray(nums []int) []int {
-	sort.Ints(nums)
+	// 1. built-in sort method
+	//sort.Ints(nums)
+	//return nums
+
+	// 2. bubble sort
+	l := len(nums)
+	for i := 0; i < l; i++ {
+		for j := 0; j < l-1-i; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
 	return nums
 }
