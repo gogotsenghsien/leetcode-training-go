c88d32a8008290947e896a724c6f679a936dedd2
kris <hsienzeng0630@gmail.com>
2021-08-14 23:05:36 +0800

    Optimize plus-one problem

--- plus-one/main.go
+++ plus-one/main.go
@@ -1,20 +1,13 @@
 package main
 
-func main() {
-	plusOne([]int{1, 2, 3})
-}
-
 func plusOne(digits []int) []int {
 	size := len(digits)
-	result := make([]int, size)
 	for i := size - 1; i >= 0; i-- {
-		digit := digits[i]
-		if digit < 9 {
+		if digits[i] < 9 {
 			digits[i]++
 			return digits
 		}
 		digits[i] = 0
 	}
-	result = append([]int{1}, digits...)
-	return result
+	return append([]int{1}, digits...)
 }
